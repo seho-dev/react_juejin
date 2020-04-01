029819230a553bc0602c7a2f3e792443e72cec2b
1018715564 <1018715564@qq.com>
2020-04-01 23:34:20 +0800

    Revert "[add]first commit"
    This reverts commit 8147b9180df0d376729fd952a8b66287103666e1.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
-import Index from "../src/pages/index/index";
-class HelloMessage extends Component {
-  render() {
-    return (<div>
-        <Index></Index>
-      </div>);
-  }
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-export default HelloMessage;
+
+export default App;
